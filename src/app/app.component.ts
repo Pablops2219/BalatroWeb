81ecdba31b4c8ec6d1cef94d26772461a406be28
Pablops <pperniassantiago@gmail.com>
2025-02-03 11:57:27 +0100

    skibididdy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,MainComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,17 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  cards = [
+    { text: 'Card 1', price: 2 },
+    { text: 'Card 2', price: 2.2 },
+    { text: 'Card 3', price: 2.3 },
+    
+  ];
 }
